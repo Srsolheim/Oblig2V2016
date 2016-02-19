60406595bbaf92461b6654e6a1d9f833a4f11475
Srsolheim <sr.solheim@gmail.com>
2016-02-19 11:20:26 +0100

    Update Kort.java

--- Oblig2_V2016/src/Kort.java
+++ Oblig2_V2016/src/Kort.java
@@ -1,44 +1,76 @@
-import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 public abstract class Kort {
-	private String fornavn;
-	private String etternavn;
+	private String fulltNavn;
 	private int pin;
+	private static int kortID = 1000;
 	private int kortnummer;
 	private boolean sperretKort;
-	private static ArrayList<Integer> alleKortnummer = new ArrayList<>();
+	private final GregorianCalendar dateCreated = new GregorianCalendar();
 	
-	Kort(String fornavn, String etternavn, int pin) {
-		this.fornavn = fornavn;
-		this.etternavn = etternavn;
+	Kort(String fulltNavn, int pin) {
+		this.fulltNavn = fulltNavn;
 		this.pin = pin;
+		kortnummer = kortID;
 		sperretKort = false;
-		setKortnummer();
 	}
 	
-	private void setKortnummer() {
-		int kortnummer = (int)(Math.random() * (10000 - 1000) + 1000);
-			while(alleKortnummer.contains(kortnummer)) {
-				kortnummer = (int)(Math.random() * (10000 - 1000) + 1000);
+	public int compareTo(Kort k) {
+		int i = this.getFulltNavn().indexOf(" "), j = k.getFulltNavn().indexOf(" ");
+		if((this.getFulltNavn().substring(i, this.getFulltNavn().length())).compareToIgnoreCase(k.getFulltNavn().substring(j, k.getFulltNavn().length())) > 0) {
+			return 1;
 		}
-		alleKortnummer.add(kortnummer);
-		this.kortnummer = kortnummer;
+		else if ((this.getFulltNavn().substring(i, this.getFulltNavn().length())).compareToIgnoreCase(k.getFulltNavn().substring(j, k.getFulltNavn().length())) < 0) {
+			return -1;
+			}
+		else {
+			if (this.getFulltNavn().substring(0, i).compareToIgnoreCase(k.getFulltNavn().substring(0, j)) > 0) {
+				return 1;
+			}
+			else if (this.getFulltNavn().substring(0, i).compareToIgnoreCase(k.getFulltNavn().substring(0, j)) < 0) {
+				return -1;
+			}
+			else {
+				return 0;
+			}	
+		}
 	}
 	
-	String getNavn() {
-		return fornavn + " " + etternavn;
+	String getFulltNavn() {
+		return fulltNavn;
+	}
+	
+	public int getKortnummer(){
+		return kortnummer;
+	}
+	public void setKortNummer(int kortNummer) {
+		this.kortnummer = kortNummer;
+	}
+	
+	public int getPin() {
+		return pin;
 	}
 	
 	boolean isSperret() {
 		return sperretKort;
 	}
 	
+	public Date getDateCreated() {
+		return dateCreated.getTime();
+	}
+	
 	@Override
 	public String toString() {
-		String header = String.format("%-20s %-20s %-20s %-20s %-20s\n", "Fornavn", "Etternavn", "Kortnummer", "PIN", "Status");
-		String information = String.format("%-20s %-20s %-20d %-20d %-20s\n", fornavn, etternavn, kortnummer, pin, ((sperretKort) ? "Sperret": "Aktiv"));
-		return header + information;
+		return "\nNavn: " + fulltNavn + "\nPIN-kode: " + pin + "\nKortnummer: " + kortnummer 
+				+ "\nKortet sperret? " + sperretKort;
 	}
 	
 	public abstract boolean sjekkPIN(int pin);
+	public abstract boolean brukerTilgang();
+	
+	public Date currentTime() {
+		GregorianCalendar timeRightNow = new GregorianCalendar();
+		return timeRightNow.getTime();
+	}
 }
